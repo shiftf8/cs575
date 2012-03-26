849ffadb1f44351f99157ae858d01bc34cc67fd0
Rob <shiftf8@gmail.com>
2012-03-26 22:58:03 +0000

    Lab4A.cpp

---
+++ Lab4.cpp
@@ -0,0 +1,69 @@
+//Lamog, Rob
+//FFFF, 2012 Lab #4A
+//Section #0245 MW 9am-11am
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+	int userInput = 0;
+	int isDivisible, isPerfect;
+	int i, j; //counters
+	
+	do {
+		/*
+			how do i check if user input is actually an integer? how do i sanitize input?
+			run a loop on each input character and check it? what about white space? null
+			characters? started looking at c_text and thought i was close when i examined
+			hex values. note to self. look into at a later date. not "required" for this
+			assignment.
+		*/
+	
+		//Check if user input is outside of 0 and 1000.		
+		if (userInput <= 0 && userInput >= 1000) {
+			cout << "You have not entered a number from 1 to 1000. Please try again or enter 0 to quit: \n";
+			cin >> userInput;
+			cout << userInput << endl;
+			
+		} else {
+			cout << "Enter a number from 1 to 1000 or enter 0 to quit: \n";
+			cin >> userInput;
+			//cout << userInput << endl;
+			
+			//Break loop if user input is zero.
+			if (userInput == 0) break;
+			//Check if user input is outside of 0 and 1000.
+			if ((userInput < 0) || (userInput > 1000)) {
+				cout << "You have not entered a number from 1 to 1000. ";
+				continue;
+			}
+		
+			//Loop through all possible divisors of user input.
+			for (i = 1, j = 0, isDivisible = 0, isPerfect = 0; userInput != i; i++) {
+				isDivisible = userInput % i;
+				if (isDivisible == 0) isPerfect = isPerfect + i;
+				j++;
+				//cout << userInput << " " << isDivisible << " " << i << " " << j << endl;
+			}
+			
+			//One is a unique number.
+			if (userInput == 1) cout << userInput << " is neither perfect or prime." << endl;
+			
+			//Check if number is prime.
+			if (j == 1) cout << userInput << " is prime." << endl;
+			
+			//Check if number is perfect.
+			if (userInput == isPerfect) cout << userInput << " is perfect." << endl;
+			
+			//Number must not be prime.
+			else if (j > 1) cout << userInput << " is not prime." << endl;
+		}
+			
+	} while (userInput != 0); //Loop until 0 is entered.
+	cout << "You have entered 0. Thank you for playing.\n";
+
+//	system("pause");
+	return 0;
+}
