396469997c4ff9f1fca656542fcfe9e2cd0ea571
Rob <shiftf8@gmail.com>
2012-04-17 09:18:23 +0000

    Successful lab using stringstream

--- Lab4.cpp
+++ Lab4.cpp
@@ -3,40 +3,51 @@
 //Section #0245 MW 9am-11am
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int main() {
     
-	int userInput = 0;
+	int userInt = 0;
+	string strInput;
+	size_t found;
 	int isDivisible, isPerfect;
 	int i, j; //counters
 	
 	do {
-		/*
-			how do i check if user input is actually an integer? how do i sanitize input?
-			run a loop on each input character and check it? what about white space? null
-			characters? started looking at c_text and thought i was close when i examined
-			hex values. note to self. look into at a later date. not "required" for this
-			assignment.
-		*/
+		cout << "Enter an integer from 1 to 1000 or \'0\' to quit: \n";
+		getline (cin, strInput);
+		//cout << strInput << endl;
 		
-		cout << "Enter an integer from 1 to 1000 or enter 0 to quit: \n";
-		cin >> userInput;
-		//cout << userInput << endl;
+		//Convert string to integer.
+		stringstream(strInput) >> userInt;
+		//cout << userInt << endl;
+		
+		//Search string input for decimal/period.
+		found=strInput.find('.');
+		
+		//Check if user input becomes zero. Means characters were encountered first. Not a valid integer.
+		if ((userInt == 0) && (strInput != "0")) {
+			cout << strInput << " is not a valid entry.\n";
+		}
+		
+		//Check if user input encounters period. Means period/decimal was found. Not a valid integer.
+		else if (found!=string::npos) {
+			cout << strInput << " is not a valid entry.\n";
+		}
 		
 		//Check if user input is outside of 0 and 1000.
-		if (userInput < 0 || userInput > 1000) {
-			cout << userInput << " is not a valid entry.\n";
-			//cout << userInput << endl;
+		else if (userInt < 0 || userInt > 1000) {
+			cout << strInput << " is not a valid entry.\n";
+			//cout << userInt << endl;
 			
 		} else {
-			//Break loop if user input is zero.
-			if (userInput == 0) break;
 		
 			//Loop through all possible divisors of user input.
-			for (i = 1, j = 0, isDivisible = 0, isPerfect = 0; userInput != i; i++) {
-				isDivisible = userInput % i; //Checking if user input is divisible.
+			for (i = 1, j = 0, isDivisible = 0, isPerfect = 0; userInt != i; i++) {
+				isDivisible = userInput % i; //Checking if user input is evenly divisible.
 				if (isDivisible == 0) {
 					isPerfect = isPerfect + i; //Checking if user input is perfect.
 					j++; //Counting number of times user input is evenly divisible.
@@ -46,18 +57,18 @@ int main() {
 			}
 			
 			//One is a unique number.
-			if (userInput == 1) cout << userInput << " is neither perfect or prime." << endl;
+			if (userInt == 1) cout << userInt << " is neither perfect or prime." << endl;
 			
-			//Check if number is prime.
-			if (j == 1) cout << userInput << " is prime." << endl;
+			//With only one even divisor number is prime.
+			if (j == 1) cout << userInt << " is prime." << endl;
 			
-			//Check if number is perfect.
-			if (userInput == isPerfect) {
-				cout << userInput << " is perfect." << endl;
+			//Number is perfect if even divisors add up to userInput.
+			if (userInt == isPerfect) {
+				cout << userInt << " is perfect." << endl;
 				cout << "Divisors include: ";
 				i = 1;
-				while (userInput != i) {
-					isDivisible = userInput % i;
+				while (userInt != i) {
+					isDivisible = userInt % i;
 					if (isDivisible == 0) {
 						cout << i << " ";
 					}
@@ -67,11 +78,11 @@ int main() {
 			
 			//Number must not be prime.
 			} else if (j > 1) {
-				cout << userInput << " is not prime." << endl;
+				cout << userInt << " is not prime." << endl;
 				cout << "Divisors include: ";
 				i = 1;
-				while (userInput != i) {
-					isDivisible = userInput % i;
+				while (userInt != i) {
+					isDivisible = userInt % i;
 					if (isDivisible == 0) {
 						cout << i << " ";
 					}
@@ -79,8 +90,8 @@ int main() {
 				}
 				cout << endl;
 			}
-		}			
-	} while (userInput != 0); //Loop until 0 is entered.
+		}
+	} while (strInput != "0"); //Loop until 0 is entered.
 	cout << "You have entered 0. Thank you for playing.\n";
 
 //	system("pause");
