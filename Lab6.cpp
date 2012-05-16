fd28edc29188a5e0b94b87ec6841638a4d9795f1
Rob <shiftf8@gmail.com>
2012-05-16 07:25:12 +0000

    Completed Lab6

---
+++ Lab6.cpp
@@ -0,0 +1,184 @@
+//Lamog, Rob
+//Lab #6A
+//Section #0245 MW 9am-11am
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+const float adultStandard = 21.75;
+const float adultDeluxe = 25.80;
+const float childDiscount = 0.60;
+const float tipAndTax = 0.18;
+const float surcharge = 0.07;
+const float discountA = 0.015;
+const float discountB = 0.025;
+const float discountC = 0.035;
+
+ifstream inData;
+ofstream outData;
+
+//Prototypes
+void check_ErrorFlags(bool error_A, bool error_C, bool error_M, bool error_S, bool error_D);
+void billing_Statement(int adultsInt, int childrenInt, bool deluxeMeal, bool weekendSurcharge, float depositFloat);
+float calc_AdultStandardMeal(int adultsInt);
+float calc_ChildrenStandardMeal(int childrenInt);
+float calc_AdultDeluxeMeal(int adultsInt);
+float calc_ChildrenDeluxeMeal(int childrenInt);
+float calc_TipandTax(float mealSubTotal);
+float calc_Surcharge(float totalMeals);
+float calc_EarlyPaymentDiscount(float totalBill);
+
+int main() {
+    string adults, children, meal, surcharge, deposit; //Data variables.
+	
+	int adultsInt, childrenInt;
+	bool deluxeMeal = false, weekendSurcharge = false;
+	float depositFloat;
+
+	bool error_A = false, error_C = false, error_M = false, error_S = false, error_D = false; //Custom error flags.
+		
+	inData.open ("C:\\Users\\Owner\\Downloads\\data.txt");
+	//Checking for error with file.
+	if (!inData) {
+		cout << "Cannot open file, terminating program." << endl;
+		exit (1);
+	}
+
+	outData.open ("C:\\Users\\Owner\\Downloads\\newfile.txt");
+
+	while (inData >> adults >> children >> meal >> surcharge >> deposit) {
+	//while (cin >> adults >> children >> meal >> surcharge >> deposit) {
+		stringstream(adults) >> adultsInt;
+		if (adultsInt < 0) error_A = true;
+		
+		stringstream(children) >> childrenInt;
+		if (childrenInt < 0) error_C = true;
+		
+		if (meal == "D") deluxeMeal = true;
+		else if (meal == "S") deluxeMeal = false;
+		else error_M = true;
+		
+		if (surcharge == "Y") weekendSurcharge = true;
+		else if (surcharge == "N") weekendSurcharge = false;
+		else error_S = true;
+		
+		stringstream(deposit) >> depositFloat;
+		if (depositFloat < 0.0) error_D = true;
+		
+		if (error_A || error_C || error_M || error_S || error_D) {
+			check_ErrorFlags(error_A, error_C, error_M, error_S, error_D);
+			//Reset custom error flags before continuing.
+			error_A = false, error_C = false, error_M = false, error_S = false, error_D = false;
+		}
+		else billing_Statement(adultsInt, childrenInt, deluxeMeal, weekendSurcharge, depositFloat);
+		//cout << adults << " " << children << " " << meal << " " << surcharge << " " << deposit << endl;
+	}
+
+	inData.close();
+	outData.close ();
+
+	system("pause");
+	return 0;
+}
+
+void check_ErrorFlags(bool error_A, bool error_C, bool error_M, bool error_S, bool error_D) {
+	if (error_A) outData << "Invalid number of adults." << endl;
+	if (error_C) outData << "Invalid number of children." << endl;
+	if (error_M) outData << "Invalid meal selection." << endl;
+	if (error_S) outData << "Invalid surcharge entry." << endl;
+	if (error_D) outData << "Invalid deposit entry." << endl;
+	outData << endl;
+}
+
+void billing_Statement(int adultsInt, int childrenInt, bool deluxeMeal, bool weekendSurcharge, float depositFloat) {
+	float adultMealTotal, childrenMealTotal, mealSubTotal, tipAndTaxTotal, surchargeTotal = 0.0, totalMeals, totalBill, balance, earlyPaymentDiscount;
+
+	//Deluxe Meal total.
+	if (deluxeMeal) {
+		//cout << "Deluxe Meal" << endl;
+		outData << "Deluxe Meal" << endl;
+		adultMealTotal = calc_AdultDeluxeMeal(adultsInt);
+		childrenMealTotal = calc_ChildrenDeluxeMeal(childrenInt);
+		mealSubTotal = calc_AdultDeluxeMeal(adultsInt) + calc_ChildrenDeluxeMeal(childrenInt);
+		//cout << "Deluxe Meal: " << mealTotal << " = (" << adultsInt << " * " << adultDeluxe << ") + ((" << childrenInt << " * " << adultDeluxe << ") / " << childDiscount << ")" << endl;
+	}
+	//Standard Meal total.
+	else {
+		//cout << "Standard Meal" << endl;
+		outData << "Standard Meal" << endl;
+		adultMealTotal = calc_AdultStandardMeal(adultsInt);
+		//cout << calc_AdultStandardMeal(adultsInt) << endl;
+		childrenMealTotal = calc_ChildrenStandardMeal(childrenInt);
+		//cout << calc_ChildrenStandardMeal(childrenInt) << endl;
+		mealSubTotal = calc_AdultStandardMeal(adultsInt) + calc_ChildrenStandardMeal(childrenInt);
+		//cout << "Standard Meal: " << mealTotal << " = (" << adultsInt << " * " << adultDeluxe << ") + ((" << childrenInt << " * " << adultDeluxe << ") / " << childDiscount << ")" << endl;
+	}
+
+	tipAndTaxTotal = calc_TipandTax(mealSubTotal);
+	totalMeals = mealSubTotal + tipAndTaxTotal;
+	if (weekendSurcharge) surchargeTotal = calc_Surcharge(totalMeals);
+	totalBill = mealSubTotal + tipAndTaxTotal + surchargeTotal;
+	balance = totalBill - depositFloat;
+	earlyPaymentDiscount = calc_EarlyPaymentDiscount(totalBill);
+	
+	//cout.precision(2);
+	outData.precision(2);
+	outData << fixed << "# of Adults: \t" << setw(4) << adultsInt << "\tCost: \t\t$" << setw(8) << adultMealTotal << endl
+		<< "# of Children: \t" << setw(4) << childrenInt << "\tCost: \t\t$" << setw(8) << childrenMealTotal << endl
+		<< "\t\t\tSubtotal: \t$" << setw(8) << mealSubTotal << endl
+		<< "\t\t\tDeposit: \t$" << setw(8) << depositFloat << endl
+		<< "\t\t\tTip and Tax: \t$" << setw(8) << tipAndTaxTotal << endl
+		<< "\t\t\tSurchage: \t$" << setw(8) << surchargeTotal << endl
+		<< "\t\t\tTotal Bill: \t$" << setw(8) << totalBill << endl << endl
+		<< "\tOutstanding Balance: $" << balance << endl
+		<< "Early payment Discount if paid within 10 days: $-" << earlyPaymentDiscount << endl << endl;
+}
+
+float calc_AdultStandardMeal(int adultsInt) {	
+	float adultMealTotal;
+	adultMealTotal = adultsInt * adultStandard;
+	return(adultMealTotal);
+}
+
+float calc_ChildrenStandardMeal(int childrenInt) {	
+	float childrenMealTotal;
+	childrenMealTotal = (childrenInt * adultStandard) / childDiscount;
+	return(childrenMealTotal);
+}
+
+float calc_AdultDeluxeMeal(int adultsInt) {
+	float adultMealTotal;
+	adultMealTotal = adultsInt * adultDeluxe;
+	return(adultMealTotal);
+}
+
+float calc_ChildrenDeluxeMeal(int childrenInt) {
+	float childrenMealTotal;
+	childrenMealTotal = (childrenInt * adultDeluxe) / childDiscount;
+	return(childrenMealTotal);
+}
+
+float calc_TipandTax(float mealSubTotal) {	
+	float tipAndTaxTotal;
+	tipAndTaxTotal = mealSubTotal * tipAndTax;
+	return(tipAndTaxTotal);
+}
+
+float calc_Surcharge(float totalMeals) {
+	float surchargeTotal;
+	surchargeTotal = totalMeals + (totalMeals * surcharge);
+	return(surchargeTotal);
+}
+
+float calc_EarlyPaymentDiscount(float totalBill) {
+	float earlyPaymentDiscount;
+	if (totalBill < 100.0) earlyPaymentDiscount = totalBill * discountA;
+	if ((totalBill >= 100.0) && (totalBill < 400.0)) earlyPaymentDiscount = totalBill * discountB;
+	if (totalBill >= 400.0) earlyPaymentDiscount = totalBill * discountC;
+	return(earlyPaymentDiscount);
+}
